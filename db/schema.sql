f813834f60e6ae4185f9c231894017a3c69e8be8
Emerson-Liwayway Recto <emersontrecto@gmail.com>
2025-08-09 22:19:16 -0400

    initial commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,36 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS post_tags;
+DROP TABLE IF EXISTS bookmarks;
 
 CREATE TABLE users (
-  id serial PRIMARY KEY,
+  user_id serial PRIMARY KEY,
   username text NOT NULL UNIQUE,
   password text NOT NULL
+);
+
+CREATE TABLE posts (
+  post_id serial PRIMARY KEY,
+  title text NOT NULL,
+  body text NOT NULL,
+  user_id int NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  created_at timestamp,
+  tag_list int []
+);
+
+CREATE TABLE tags (
+  tag_id serial PRIMARY KEY,
+  tag_name text UNIQUE
+);
+
+CREATE TABLE post_tags (
+  post_id int REFERENCES posts(post_id) ON DELETE CASCADE,
+  tag_id int REFERENCES tags(tag_id) ON DELETE CASCADE
+);
+
+CREATE TABLE bookmarks (
+  user_id int REFERENCES users(user_id) ON DELETE CASCADE,
+  post_id int REFERENCES posts(post_id) ON DELETE CASCADE,
+  PRIMARY KEY (user_id, post_id)
 );
